cffad3f6d5d94e80470a40878ce6ab5de27bc5ce
camille <tal.adoum@gmail.com>
2017-05-03 11:11:56 +0200

    Classe CompagnieAerienne terminee1

---
+++ src/main/java/agence/model/CompagnieAerienne.java
@@ -0,0 +1,31 @@
+package agence.model;
+
+public class CompagnieAerienne 
+{
+	private int idCAerienne;
+	private String nom;
+	private Vol vol;
+	public int getIdCAerienne() {
+		return idCAerienne;
+	}
+	public void setIdCAerienne(int idCAerienne) {
+		this.idCAerienne = idCAerienne;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Vol getVol() {
+		return vol;
+	}
+	public void setVol(Vol vol) {
+		this.vol = vol;
+	}
+	@Override
+	public String toString() {
+		return "CompagnieAerienne [idCAerienne=" + idCAerienne + ", nom=" + nom + ", vol=" + vol + "]";
+	}
+	
+}
