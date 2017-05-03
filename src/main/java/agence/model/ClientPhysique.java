2842da2d63940e470fd5a0eb425286494913ac26
anaislancry <anaislancry@yahoo.fr>
2017-05-03 11:10:01 +0200

    Creation classe fille ClientPhysique
    debut

--- src/main/java/agence/model/ClientPhysique.java
+++ src/main/java/agence/model/ClientPhysique.java
@@ -2,6 +2,40 @@ package agence.model;
 
 public class ClientPhysique extends Client {
 
-	private String prenom
+	private String prenom;
 	
+	public ClientPhysique(String prenom)
+    {
+        this.prenom = prenom;
+    }
+
+    // constructeurs
+    
+    public ClientPhysique() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "ClientPhysique [prenom=" + prenom + "]";
+	}
+	
+    
 }
