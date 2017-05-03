9ce463a58eecc95f15062407c0a897281a74d4e3
Unknown <plateau.romain@gmail.com>
2017-05-03 10:58:38 +0200

    Classe Aeroport

---
+++ src/main/java/agence/model/Aeroport.java
@@ -0,0 +1,34 @@
+package agence.model;
+
+public class Aeroport {
+
+	private int idAero;
+	private String nom;
+	
+	//Constructeur par défault sans paramètre
+	public Aeroport() {
+		
+	}
+	// Constructeur avec deu paramètres, id et nom
+	public Aeroport(int idAero, String nom) {
+		super();
+		this.idAero = idAero;
+		this.nom = nom;
+	}
+	//Getter et Setter idAero
+	public int getIdAero() {
+		return idAero;
+	}
+	public void setIdAero(int idAero) {
+		this.idAero = idAero;
+	}
+	
+	//Getter et Setter nom
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+}
